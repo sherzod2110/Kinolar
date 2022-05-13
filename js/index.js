cfb7edb47a98b66bd7e029b935bbe3bcf0d85491
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-05-13 22:30:32 +0500

    gffggf

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ kinolar.splice(100);
 kinolar.forEach(function (kino) {
   
   let newLiItem = createElement("li", "list-item", "");
-  let newTitle = createElement("p", "item-name", kino.title.toUpperCase());
+  let newTitle = createElement("h3", "item-name", kino.title.toUpperCase());
+  let newBox = createElement("div", "box-1",);
   let newYear = createElement("p", "item-year", kino.year);
   let newCast = createElement("p", "item-cast", kino.cast);
   let newGenres = createElement("p", "item-genres", kino.genres.toString());
@@ -14,6 +15,8 @@ kinolar.forEach(function (kino) {
   newGenres.textContent = kino.genres.join(", ");
   newCast.textContent = kino.cast.join(", ");
   
-  newLiItem.append(newTitle, newYear, newCast, newGenres);
+  newBox.append(newYear, newCast);
+  newLiItem.append(newTitle, newBox, newGenres);
+  // newLiItem.append(newTitle, newYear, newCast, newGenres);
   elList.appendChild(newLiItem);
 })
